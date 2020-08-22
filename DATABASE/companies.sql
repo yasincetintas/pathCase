b0f6cf78826a4a42e6fab5ed3f5fd3f54b8b9989
yasincetintas <ysnctnts@gmail.com>
2020-08-22 22:38:05 +0300

    Database Datas

---
+++ DATABASE/companies.sql
@@ -0,0 +1,1 @@
+INSERT INTO "companies"("id", "name", "created_at", "updated_at", "deleted_at") VALUES (1, 'ABC', '2020-08-22 17:30:12', '2020-08-22 17:30:12', NULL);
---
+++ DATABASE/products.sql
@@ -0,0 +1,4 @@
+INSERT INTO "products"("id", "name", "available_pieces", "created_at", "updated_at", "deleted_at") VALUES (2, 'armut', 10, NULL, NULL, NULL);
+INSERT INTO "products"("id", "name", "available_pieces", "created_at", "updated_at", "deleted_at") VALUES (3, 'muz', 10, NULL, NULL, NULL);
+INSERT INTO "products"("id", "name", "available_pieces", "created_at", "updated_at", "deleted_at") VALUES (4, 'macbook air', 10, NULL, NULL, NULL);
+INSERT INTO "products"("id", "name", "available_pieces", "created_at", "updated_at", "deleted_at") VALUES (1, 'elma', 1, NULL, '2020-08-22 22:18:21', NULL);
